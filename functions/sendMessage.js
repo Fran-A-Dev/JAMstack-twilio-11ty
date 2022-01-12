87ff601b0274f14180183db1a1f725c02a3bf5cb
Francis Agulto <francisagulto@franciss-MBP.attlocal.net>
2022-01-12 09:15:36 -0600

    needs netlify cli

--- functions/sendMessage.js
+++ functions/sendMessage.js
@@ -2,8 +2,8 @@
 Get your Twilio accountSid, authToken and phone number after logging in. More details
 on https://www.twilio.com/docs
 */
-const accountSid = "AC07faba98ea798a2ba8d19e2f30ae45fd";
-const authToken = "79240cb04d1f9ce934760ea317b029fa";
+const accountSid = "";
+const authToken = "";
 const client = require("twilio")(accountSid, authToken);
 
 module.exports.handler = async (event) => {
@@ -16,7 +16,7 @@ module.exports.handler = async (event) => {
   try {
     await client.messages.create({
       body: payload.message,
-      from: "+19592142428", // your Twilio Phone number (https://www.twilio.com/docs/phone-numbers)
+      from: "", // your Twilio Phone number (https://www.twilio.com/docs/phone-numbers)
       to: payload.to,
     });
     return {
